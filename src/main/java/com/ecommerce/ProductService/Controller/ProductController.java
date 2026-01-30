62f0289f0563cf8e123715a001014331c9900ab5
Shivkumar Shinde <shindeshivkumsr33@gmail.com>
2026-01-30 13:42:02 +0530

    added code for creating user class and establish db connection and connect hibernate.

--- src/main/java/com/ecommerce/ProductService/Controller/ProductController.java
+++ src/main/java/com/ecommerce/ProductService/Controller/ProductController.java
@@ -1,9 +1,12 @@
 package com.ecommerce.ProductService.Controller;
 
 import com.ecommerce.ProductService.DTOs.ProductDto;
+import com.ecommerce.ProductService.Exceptions.NotFoundException;
 import com.ecommerce.ProductService.Models.Product;
 import com.ecommerce.ProductService.Service.ProductService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
@@ -13,8 +16,14 @@ public class ProductController {
         this.productService=productService;
     }
 
+    @GetMapping("")
+    public @ResponseBody Product[] getProducts(){
+        return productService.getProducts();
+    }
+
+
     @GetMapping("/{id}")
-    public @ResponseBody ProductDto getProductById(@PathVariable("id") Long id){
+    public @ResponseBody ProductDto getProductById(@PathVariable("id") Long id) throws NotFoundException {
         return productService.getProductDto();
     }
     @PostMapping("")
--- src/main/java/com/ecommerce/ProductService/Service/ProductService.java
+++ src/main/java/com/ecommerce/ProductService/Service/ProductService.java
@@ -1,17 +1,23 @@
 package com.ecommerce.ProductService.Service;
 
 import com.ecommerce.ProductService.DTOs.ProductDto;
+import com.ecommerce.ProductService.Exceptions.NotFoundException;
 import com.ecommerce.ProductService.Models.Product;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 @Service
 public class ProductService {
 
-    public ProductDto getProductDto(){
+    public ProductDto getProductDto() throws NotFoundException {
         RestTemplate restTemplate = new RestTemplate();
-        String apiUrl = "https://fakestoreapi.com/products/1";
+        String apiUrl = "https://fakestoreapi.com/products/21";
         Product product = restTemplate.getForObject(apiUrl,Product.class);
+        if(product==null) throw new NotFoundException();
         System.out.println(product);
 
         ProductDto obj = new ProductDto();
@@ -21,4 +27,10 @@ public class ProductService {
         return obj;
     }
 
+    public  Product[] getProducts() {
+        RestTemplate restTemplate = new RestTemplate();
+        String apiUrl = "https://fakestoreapi.com/products";
+        Product[] products = restTemplate.getForObject(apiUrl, Product[].class);
+        return products;
+    }
 }
